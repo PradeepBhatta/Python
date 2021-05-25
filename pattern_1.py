376afcf486534c3f316bcd2f61865d23ae45b199
Pradeep Bhatta <75995879+PradeepBhatta@users.noreply.github.com>
2021-05-25 21:23:25 +0545

    Update pattern_1.py

--- pattern_1.py
+++ pattern_1.py
@@ -1,3 +1,14 @@
+"""
+Program to print the following series"
+
+10 20 30 40 50  
+20 30 40 50 60  
+30 40 50 60 70  
+40 50 60 70 80  
+50 60 70 80 90  
+
+"""
+
 for i in range(5):
     for j in range(5):
         print((i+j+1)*10,end=" ")
