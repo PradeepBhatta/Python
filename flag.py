1aea7ad9e28cdd5a813e6b228f2b32fe3174775d
Pradeep Bhatta <75995879+PradeepBhatta@users.noreply.github.com>
2021-05-25 21:13:49 +0545

    Create flag.py

---
+++ flag.py
@@ -0,0 +1,20 @@
+import time
+
+for i in range(10):
+    for j in range(i):
+        print("*",end="")
+        time.sleep(0.01)
+    print("")
+    time.sleep(0.01)
+    
+for i in range(10):
+    for j in range(i+2):
+        print("*",end="")
+        time.sleep(0.01)
+    print("")
+    time.sleep(0.01)
+
+for k in range(5):
+    print("*")
+    time.sleep(0.01)
+    
