ee4eaba4bcca58695007794573b9ca921c64a6cb
Pradeep Bhatta <75995879+PradeepBhatta@users.noreply.github.com>
2021-05-25 21:30:11 +0545

    Create pattern_4.py

---
+++ pattern_4.py
@@ -0,0 +1,21 @@
+"""
+Program to prnt the following series
+
+# # # # # 
+# # # # # 
+# # # # # 
+# # # # # 
+# # # # # 
+
+
+"""
+
+def abc():
+    for j in range(5):
+        for i in range(5):
+            print("#",end=" ")
+        print("")
+    return
+
+abc()
+
